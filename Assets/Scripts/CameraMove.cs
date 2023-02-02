9f7b39c90cac36aba250b6faf3325422d0f52ef8
Johann Mayos Lopes da Silva <42842871+JohannMayos@users.noreply.github.com>
2023-02-02 10:32:55 -0400

    Parallax Effects Added

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed = 3f;
+    private Transform cam;
+
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = cam.position;
+        newPosition.x += cameraSpeed * Time.deltaTime;
+        cam.position = newPosition;
+    }
+}
